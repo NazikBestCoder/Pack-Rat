71b981cd3bbdf6742ae4d19dae85d11db332ded7
lamperouge <philbohn1791@gmail.com>
2021-09-23 14:22:06 -0400

    got API pulls hooked up and providing proper data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,18 +4,16 @@ var oWApiKey = "22699aa722b7a79950a6f5dfaa6a318e";
 var bodyEl = $(".body");
 var lat;
 var lon;
-var stateCode;
 var searchBtn;
 var dropCont = $(".select");
 var checkList = $(".checklist");
 var weatherEl = $(".weather");
 var nasaURL = "https://api.nasa.gov/planetary/earth/imagery?lon=-110.5471695&lat=44.59824417&date=2020-09-22&api_key=" + nasaApiKey;
-var natParkURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateCode + "&api_key=" + natParkApiKey;
 var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&appid=" + oWApiKey;
 
 
 // General path forward:
-    // 1. State input and then park code input
+    // 1. State input [STATE INPUT WORKS AND LOGS PROPERLY] and then park code input
     // 2. Call function from NPS, based on state code input 
         // 2a. This brings up a list of the parks that are in that state, with name and park code
     // 3. Call function for the state park that is on the button click from the park code input search
@@ -23,10 +21,27 @@ var weatherURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + 
     // 4. Call function for weather based on the lat/lon from the state park
     // 5. Call function for the satellite image based on the lat/lon from the state park 
 
-dropCont.on("change", function(){
+dropCont.on("change", function () {
     console.log($(this).val());
+    var stateCode = $(this).val();
+    var natParkURL = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateCode + "&api_key=" + natParkApiKey;
+
+    fetch(natParkURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+
+    
+    
 });
 
+function parkCodeHandler() {
+    $("<select>");
+    
+}
 
 
 
